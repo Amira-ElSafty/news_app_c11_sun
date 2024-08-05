1aaadb86c80b59669f6ac9b60c551639bce24874
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-08-05 17:10:29 +0300

    finished splash screen in light and dark mode.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_news_c11_sun/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(children: [
+      Container(
+        color: AppColors.whiteColor,
+        child: Image.asset(
+          'assets/images/main_background.png',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+        ),
+      ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            'News App',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+      )
+    ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_news_c11_sun/home/home_screen.dart';
+import 'package:flutter_app_news_c11_sun/my_theme_data.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: MyThemeData.lightTheme,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
